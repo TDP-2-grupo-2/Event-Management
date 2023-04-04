00d83a897be1e344fd93dcf37a6821748f975494
agustina235 <asegura@fi.uba.ar>
2023-04-04 17:40:47 -0300

    Feat: create form for event
    Co-authored-by: Sol Fontenla <solfonte@users.noreply.github.com>

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import { Paper, Grid, Typography, TextField } from "@mui/material";
+import React , {useState, useEffect } from "react";
 
+
+
+const initialValues = {
+    eventName: "",
+    description: "",
+}
 
 export const CreateEvent = () => {
     console.log("entreeeee")
+    const [values, setValues] =useState(initialValues)
+  
+
     return (
-        <div>
-            <h1> Create Event Pages</h1>
+        <div className="CreateEvent">
+            <Typography gutterBottom variant="h3" align="center">
+                Create Event
+            </Typography>
+            
+            <Grid container spacing={8}  >
+                    <Grid  item xs={12}>
+                        <TextField label="Nombre Evento" 
+                        placeholder="Ingresa el nombre del evento" 
+                        variant="outlined" 
+                        value= {values.eventName}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField label="Descripcion" 
+                                    placeholder="Ingresa la descripcion del evento" 
+                                    variant="outlined"  
+                                    value={values.description}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField label="Descripcion" 
+                                    placeholder="Ingresa la descripcion del evento" 
+                                    variant="outlined"  
+                                    value={values.description}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField label="Descripcion" 
+                                    placeholder="Ingresa la descripcion del evento" 
+                                    variant="outlined"  
+                                    value={values.description}/>
+                    </Grid>
+            </Grid>
+                
         </div>
+
     );
+   
+    
 };
