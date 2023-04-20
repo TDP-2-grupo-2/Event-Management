3504015371e2123e064135c73806ee646b7e0317
agustina235 <asegura@fi.uba.ar>
2023-04-20 17:55:58 -0300

    Feat: conexion to google

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import AppRoutes from './AppRoutes';
 import {Sidebar} from './components/Sidebar'
+import {Login} from './pages/Login'
+import { useState } from 'react';
 
 function App() {
+  let [authenticated, setAuthentification] = useState(false)
   return (
-    <BrowserRouter>
-      <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      <Sidebar>
-      </Sidebar>
-    </BrowserRouter>
-  
+      
+      authenticated ?
+        <BrowserRouter>
+          <Routes>
+              {AppRoutes.map((route, index) => {
+                const { element, ...rest } = route;
+                return <Route key={index} {...rest} element={element} />;
+              })}
+            </Routes>
+        
+          <Sidebar>
+          </Sidebar>
+        </BrowserRouter>
+      :
+      <Login 
+            authenticated={authenticated} 
+            setAuthentification={setAuthentification}>
+      </Login>
+
+      
   );
 }
 
